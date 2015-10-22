e7833ba0f2f3f39d657d21f7003e818e0f94bff5
Tero Laitinen <tero.laitinen@iki.fi>
2015-10-22 14:38:56 +0000

    image zooming with EasyZoom

--- frontend/app/view/main/MainController.js
+++ frontend/app/view/main/MainController.js
@@ -95,13 +95,13 @@ Ext.define('Receipts.view.main.MainController', {
             },
 
             'receiptsgrid button[name=send]': {
-                click: function(button) {
+                click: function(sendButton) {
                     Ext.MessageBox.confirm(__('confirmsend.title'), __('confirmsend.message'),
                         function (button) {
                         if (button == "yes") {
-                            var ppCombo = controller.getProcessPeriodCombo();
+                            var processPeriodId = sendButton.up('grid').processPeriodId;
                             Ext.Ajax.request({
-                                url: 'backend/db/processperiods/' + ppCombo.getValue(),
+                                url: 'backend/db/processperiods/' + processPeriodId,
                                 method: 'POST',
                                 params: {
                                 }
--- frontend/app/view/main/Preview.js
+++ frontend/app/view/main/Preview.js
@@ -2,11 +2,13 @@ Ext.define('Receipts.view.main.Preview', {
     alias: 'widget.receiptpreview',
     extend: 'Ext.Panel',
     layout:'fit',
-    setPreviewFileId: function(fileId) {
-        if (fileId != null)
-            this.setHtml('<img style="width:100%; height:100%" src="backend/file/' + fileId + '"/>');
-        else
-            this.setHtml('');
+    setPreviewFileId: function(fileId) { 
+        if (fileId != null) {
+            var div = $('<div style="width:50%; height:100%" class="easyzoom easyzoom--adjacent is-ready"><a href="backend/file/' + fileId +'"><img style="width:100%; height:100%" src="backend/file/' + fileId + '"/></a></div>');
+            div.easyZoom();
+            $(this.getEl().dom).empty().append(div);
+        } else
+            this.setHtml('<div style="background: #eee; width:100%; height:100%"/>');
 
     }
 });
