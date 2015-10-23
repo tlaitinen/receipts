9ceb19a2bd41cd0fb5b26d78b62171721508c4fd
Tero Laitinen <tero.laitinen@iki.fi>
2015-10-23 12:34:55 +0000

    fixed ReceiptPreview view bug

--- frontend/app/view/main/Preview.js
+++ frontend/app/view/main/Preview.js
@@ -3,16 +3,16 @@ Ext.define('Receipts.view.main.Preview', {
     extend: 'Ext.Panel',
     layout:'fit',
     setDownloadLink: function(fileId) {
-       $(this.getEl().dom).empty()
+       $(this.body.dom).empty()
         .append('<div style="width:100%; height:100%; text-align:center;"><a target="_blank" href="backend/file/' + fileId  +'">' + __('preview.notAvailable') +  '</a></div>');
     },
     setPreviewFileId: function(fileId) { 
         if (fileId != null) {
             var div = $('<div style="width:50%; height:100%" class="easyzoom easyzoom--adjacent is-ready"><a href="backend/file/' + fileId +'"><img style="width:100%; height:100%" src="backend/file/' + fileId + '"/></a></div>');
             div.easyZoom();
-            $(this.getEl().dom).empty().append(div);
+            $(this.body.dom).empty().append(div);
         } else
-            $(this.getEl().dom).empty().append($('<div style="background: #eee; width:100%; height:100%"/>'));
+            $(this.body.dom).empty().append($('<div style="background: #eee; width:100%; height:100%"/>'));
 
     }
 });
